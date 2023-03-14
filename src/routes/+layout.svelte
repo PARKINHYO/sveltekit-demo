aaa63f2e21ab7379883c1958be00ad746521e52f
PARKINHYO <inhyopark122@gmail.com>
2023-03-14 19:06:22 +0900

    chore: github pages test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,4 @@
 <main>
     <slot />
 </main>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,9 +5,7 @@
     export let data;
 </script>
 
-<h1>Home</h1>
-
-<form method="POST" action="?/signup"><button>Sign up</button></form>
+<!-- <form method="POST" action="?/signup"><button>Sign up</button></form>
 <br><br>
 
 {#if !data?.sessionid}
@@ -33,4 +31,135 @@
 
 {#if form?.invalidMessage}
 <p>{form.invalidMessage}</p>
-{/if}
+{/if} -->
+
+<!-- <style>
+    body {
+        background-color: #0C0E12;
+        overflow-y: scroll;
+    }
+    html {
+        overflow-y: scroll;
+    }
+</style> -->
+<div class="main3"><a href="/" ><img src="logo.png" alt="logo" width="65" height="65"></a></div>
+
+<div class="main4">Sign in to KS5</div>
+
+
+<div class="main">
+    <!-- <h1 class="logo">Acidgram</h1> -->
+    <div class="container">
+        <div class="container2">Id</div>
+        <input type="text" id="id" class="account">
+        <div class="container2">Password</div>
+        <input type="password" id="password" class="account">
+        <button id="login" class="account">Sign in</button>
+    </div>
+</div>   
+<div class="main2">
+    <div class="container2">New to KS5? <a href="/signup" class="link">Create an account</a>.</div>
+</div>
+
+<style>
+.main {
+    width: 310px;
+    margin: auto;
+    margin-top: 10px;
+    height: 220px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    /* border: 1px solid lightgrey; */
+    border-radius: 10px;
+    background-color: #121519;
+    border: 1px solid #191D21;
+}
+
+/* .logo {
+    margin-top: 0px;
+    margin-bottom: 40px;
+} */
+
+#login {
+    width: 100%;
+    background-color: #1F7629;
+    border-color: transparent;
+    color: white;
+    margin-top: 15px;
+}
+
+.account {
+    width: 275px;
+    height: 29px;
+    display: block;
+    margin-bottom: 2px;
+    /* padding: 6px; */
+    border: 1px solid #24292E;
+    border-radius: 7px;
+    background-color: #0C0E12;
+    color: white;
+}
+
+.container2 {
+    padding: 6px;
+}
+
+.main2 {
+    width: 310px;
+    margin: auto;
+    margin-top: 12px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    /* border: 1px solid lightgrey; */
+    border-radius: 10px;
+    background-color: #0C0E12;
+    border: 1px solid #24292E;
+}
+
+.main3 {
+    width: 170px;
+    margin: auto;
+    margin-top: 35px;
+    height: 70px;
+    font-size: 28px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    /* border: 1px solid lightgrey; */
+    border-radius: 10px;
+    background-color: #0B0C0F;
+}
+
+.main4 {
+    width: 170px;
+    margin: auto;
+    margin-top: 3px;
+    height: 70px;
+    font-size: 28px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    /* border: 1px solid lightgrey; */
+    border-radius: 10px;
+    background-color: #0B0C0F;
+}
+
+.link {
+    color: #407CDC;
+    text-decoration-line: none;
+}
+.link:hover {
+    text-decoration: underline;
+}
+
+/* #alert {
+    border-color: transparent;
+} */
+</style>
